366d7f40a2167c206673b9db08629a800aaef284
Danish vhora <133424243+DanishVahora@users.noreply.github.com>
2025-02-16 15:00:44 +0530

    Update GamePage.tsx

--- chess_game/src/GamePage.tsx
+++ chess_game/src/GamePage.tsx
@@ -1,4 +1,4 @@
-// GamePage.tsx
+
 import React, { useState, useEffect, useMemo } from "react";
 import { io, Socket } from "socket.io-client";
 
@@ -15,14 +15,14 @@ const GamePage: React.FC = () => {
 
   useEffect(() => {
     const handleConnect = () => {
-      setSocketId(socket.id ?? null); // Ensure undefined doesn't cause an issue
+      setSocketId(socket.id ?? null); 
       console.log("Connected:", socket.id);
     };
 
     socket.on("connect", handleConnect);
 
     return () => {
-      socket.off("connect", handleConnect); // Cleanup event listener on unmount
+      socket.off("connect", handleConnect); 
     };
   }, [socket]);
 
