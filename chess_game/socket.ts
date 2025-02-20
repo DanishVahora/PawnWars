f3bcdb34232cf4da8ddff6cedc6376629b85f817
Danish vhora <133424243+DanishVahora@users.noreply.github.com>
2025-02-20 11:54:49 +0545

    Update socket.ts

--- chess_game/socket.ts
+++ chess_game/socket.ts
@@ -2,9 +2,10 @@
 import { io } from "socket.io-client";
 
 // Create a single socket instance for the entire app
-const socket = io(import.meta.env.VITE_BACKEND_URL?.trim() || "http://localhost:5000", {
+const socket = io(process.env.NEXT_PUBLIC_BACKEND_URL?.trim() || "http://localhost:5000", {
   withCredentials: true,
 });
+
 
 
 export default socket;
