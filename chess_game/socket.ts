2bcbcc293a3b46554065b74d4845167f8e907533
Danish vhora <133424243+DanishVahora@users.noreply.github.com>
2025-02-20 12:34:57 +0545

    Update socket.ts

--- chess_game/socket.ts
+++ chess_game/socket.ts
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 
 // Create a single socket instance for the entire app
 const socket = io(import.meta.env.VITE_BACKEND_URL || "http://localhost:5000", {
+  transports: ["websocket", "polling"],
   withCredentials: true,
 });
 
